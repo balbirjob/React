1a606b2330418faaee5aaa89d364df6f18f70e0d
Balbir <balbirjob@gmail.com>
2024-02-02 03:14:12 +0530

    react day 1
    basic

---
+++ project/src/App.js
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello i will lean react  </h1>
+      <br/>
+      <p>Heee</p>
+    </div>
+    
+    
+  );
+}
+export default App;
+
+// function App1(){
+//   return(
+//     <h1>how are you react</h1>
+//   );
+// }
+//  App1();
+
+
---
+++ project/src/My.jsx
@@ -0,0 +1,8 @@
+function App1(){
+    return(
+        <div>
+           <h1> Helloo My.JSX is here</h1>
+        </div>
+    );
+}
+export default App1;
